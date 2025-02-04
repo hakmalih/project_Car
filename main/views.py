931a27d1d78f04ac521ed44e68aa6644944f0e3f
hakmalih <annaseredins@gmail.com>
2025-02-04 13:02:23 +0300

    вывод данных из бд

--- main/views.py
+++ main/views.py
@@ -7,10 +7,6 @@ from main.models import Car, Brand
 def get_main_page(request):
     cars =Car.objects.all()
     brands=Brand.objects.all()
-    data=request.GET.get('options')
-    if data:
-        cars = Car.objects.filter(brand=data)
-    bmw=brands[1]
     context = {
         'cars': cars,
         'brands':brands,

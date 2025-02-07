febc16782732236dbe589daa7edccaf5fc68732b
hakmalih <annaseredins@gmail.com>
2025-02-07 14:03:56 +0300

    загрузка 4

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import get_main_page, car_detail
+from .views import get_main_page, car_detail, add_car
 
 urlpatterns = [
     path('',get_main_page),
     path('car_detail/<int:id>',car_detail,name='car_detail'),
+    path('add_car',add_car,name='add_car'),
 ]
--- main/views.py
+++ main/views.py
@@ -6,10 +6,8 @@ from main.models import Car, Brand, CarImage
 # Create your views here.
 def get_main_page(request):
     cars =Car.objects.all()
-    brands=Brand.objects.all()
     context = {
         'cards': cars,
-        'brands':brands,
     }
     return render(request,'main.html', context)
 
@@ -17,6 +15,8 @@ def get_main_page(request):
 def car_detail(request,id):
     cars =Car.objects.get(id=id)
     picture = CarImage.objects.filter(car=id)
+    print(cars)
+    print(picture)
     context = {
         'cars': cars,
         'picture':picture,
@@ -25,3 +25,27 @@ def car_detail(request,id):
 
 
 
+def add_car(request):
+    new =Car.objects.all()
+    brands=Brand.objects.all()
+    context = {
+        'new': new,
+        'brands':brands,
+    }
+    if request.method == 'POST':
+        data = request.POST
+        model = data.get('model')
+        color= data.get('color')
+        annotation= data.get('annotation')
+        description= data.get('description')
+        price = data.get('price')
+        image = data.get('image')
+        if model and color and annotation and description and image:
+            if price.isdigit() :
+                new_car_instance = Car(model=model,color=color,annotation=annotation,description=description,price=price,image=image)
+                new_car_instance.save()
+            else:
+                context['error_age'] = 'неправильный тип данных '
+        else:
+            context['error'] = 'заполните все поля'
+    return render(request,'add_car.html', context)

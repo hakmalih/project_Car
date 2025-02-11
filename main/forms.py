1b6584ee15b1dedb99116149a24e4ca59f2edb26
hakmalih <annaseredins@gmail.com>
2025-02-11 21:54:08 +0300

    загрузка form

--- main/forms.py
+++ main/forms.py
@@ -1,12 +1,29 @@
 from django import forms
 from .models import Brand
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.ImageField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
 
 class AddCarForm(forms.Form):
     brand = forms.ModelChoiceField(queryset=Brand.objects.all())
     model = forms.CharField(max_length=50)
     price = forms.DecimalField(max_digits=8, decimal_places=2)
     image = forms.ImageField()
+    additional_img = MultipleFileField()
     color = forms.CharField(max_length=100)
     description = forms.CharField(widget=forms.Textarea())
     annotation = forms.CharField(max_length=1000)
--- main/views.py
+++ main/views.py
@@ -7,38 +7,44 @@ from .forms import AddCarForm
 
 # Create your views here.
 def get_main_page(request):
-    cars =Car.objects.all()
+    cars = Car.objects.all()
     context = {
         'cards': cars,
     }
-    return render(request,'main.html', context)
+    return render(request, 'main.html', context)
 
 
-def car_detail(request,id):
-    cars =Car.objects.get(id=id)
+def car_detail(request, id):
+    cars = Car.objects.get(id=id)
     picture = CarImage.objects.filter(car=id)
     print(cars)
     print(picture)
     context = {
         'cars': cars,
-        'picture':picture,
+        'picture': picture,
     }
-    return render(request,'car_detail.html', context)
-
+    return render(request, 'car_detail.html', context)
 
 
 def add_car(request):
     form = AddCarForm()
-    print(form.is_bound,'32')
-    if request.method=="POST":
-        form = AddCarForm(request.POST,request.FILES)
+    print(form.is_bound, '32')
+    if request.method == "POST":
+        form = AddCarForm(request.POST, request.FILES)
         if form.is_valid():
-            print('ok')
             print(form.cleaned_data)
-        else:
-            print('ne ok')
-            print(form.errors)
-    context={
-        'form':form,
+            new_car = Car(brand=form.cleaned_data.get('brand'),
+                          model=form.cleaned_data.get('model'),
+                          price=form.cleaned_data.get('price'),
+                          image=form.cleaned_data.get('image'),
+                          color=form.cleaned_data.get('color'),
+                          description=form.cleaned_data.get('description'),
+                          annotation=form.cleaned_data.get('annotation'))
+            new_car.save()
+            for image in form.cleaned_data.get('additional_img'):
+                additional_img=CarImage(picture=image,car_id=new_car.id)
+                additional_img.save()
+    context = {
+        'form': form,
     }
-    return render(request,'add_car.html', context)
+    return render(request, 'add_car.html', context)
